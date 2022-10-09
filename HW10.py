4cfa4637d40f4f03473f290a3290d364c314cfcc
SergiiOshmarin <113200315+SergiiOshmarin@users.noreply.github.com>
2022-10-09 18:40:13 +0200

    changes as requested

--- HW10.py
+++ HW10.py
@@ -18,9 +18,9 @@ class Record():
         self.phones.append(Phone(phone))
     
     def delete_phone(self, phone):
-        phone_obj = Phone(phone)    
-        if phone_obj in self.phones:
-            self.phone.remove(phone_obj)
+        some_list=[phone.value for phone in self.phones]
+        if phone in some_list:
+            self.phones.pop(some_list.index(phone))
 
     def change_phone(self, old_phone, new_phone):
         some_list=[phone.value for phone in self.phones]
@@ -68,7 +68,7 @@ def sanitize_phone_number(phone):
     return new_phone
 
 def format_name(list):
-    if type(list)==str:
+    if isinstance(list,str):
         list=list.strip().split()
     new_name=[]
     for i in list:
@@ -86,7 +86,7 @@ def parser_command(string):
 
 def name_search(some_list):
     name_list=list(contacts_dictionary.data.keys())
-    if type(some_list)==str:
+    if isinstance(some_list,str):
         some_list=some_list.strip().split()
     result=[]
     if len(some_list)==1:
@@ -117,84 +117,97 @@ def name_search(some_list):
         return name_search(input(f'{result} These are the best match what I found for you. Type one of this names to see number you need\n'))
     else:
         return name_search(input('No results were found with such name.Try another name or type "cancel"\n'))
-    
+def hello():
+    return handler(input("Hello.How can I help you?:\n"))
+def add(list):
+    if isinstance(list,str):
+        add(["add"]+parser_command(list))
+    elif len(list)==4:
+        list2=[]
+        list2.extend(list[1:-1])
+        clean_name=format_name(list2)
+        clean_name2=format_name(" ".join([list2[1],list2[0]]))
+        if clean_name in contacts_dictionary:
+            record = contacts_dictionary.data[clean_name]
+            record.add_phone(sanitize_phone_number(list[-1]))
+        elif clean_name2 in contacts_dictionary:
+            record = contacts_dictionary.data[clean_name2]
+            record.add_phone(sanitize_phone_number(list[-1]))
+        else:
+            record = Record(clean_name, sanitize_phone_number(list[-1]))
+            contacts_dictionary.add_record(record)
+        print(f'Contact dictionary successfully updated with such contact {clean_name}:{sanitize_phone_number(list[-1])}')
+    else:
+        return add(input('Something wrong.Please type first name,second name and phone number:\n'))
+def phone_number(list):
+    if isinstance(list,str):
+        phone_number(["phone"]+parser_command(list))
+    elif len(list)<2 or len(list)>4:
+        return phone_number(input('Function "change" accept only 2 parameters.Please type first name and second name.\n'))
+    elif len(list) in [2,3]:
+        list2=list[1:]
+        phone_number=name_search(list2)
+        if phone_number==None:
+            print("Let's try something else")
+        else:
+            print(f'Contact has such number(s):{phone_number}')
+def change(list):
+    if isinstance(list,str):
+        change(["change"]+parser_command(list))
+    elif len(list)==4:
+        list2=list[1:3]
+        clean_name=format_name(list2)
+        clean_name2=format_name(" ".join([list2[1],list2[0]]))
+        if clean_name in contacts_dictionary.data.keys():
+            old_phone=contacts_dictionary.data[clean_name]
+            if len(old_phone.phones)>=2:
+                old_telephone=input(f"There are few phones for {clean_name}.{[phone.value for phone in old_phone.phones]}.Type one you want to change.")
+                old_phone.change_phone(sanitize_phone_number(old_telephone), sanitize_phone_number(list[-1]))
+                print(f'Contact dictionary successfully updated. {old_telephone} replaced with {sanitize_phone_number(list[-1])}')
+            else:
+                old_phone.change_phone(old_phone.phones[0].value, sanitize_phone_number(list[-1]))
+                print(f'Contact dictionary successfully updated. Old phone replaced with {sanitize_phone_number(list[-1])}')
+        elif clean_name2 in contacts_dictionary.data.keys():
+            old_phone=contacts_dictionary.data[clean_name2]
+            if len(old_phone.phones)>=2:
+                old_telephone=input(f"There are few phones for {clean_name2}.{[phone.value for phone in old_phone.phones]}.Type one you want to change.")
+                old_phone.change_phone(sanitize_phone_number(old_telephone), sanitize_phone_number(list[-1]))
+                print(f'Contact dictionary successfully updated. {old_telephone} replaced with {sanitize_phone_number(list[-1])}')
+            else:
+                old_phone.change_phone(old_phone.phones[0].value, sanitize_phone_number(list[-1]))
+                print(f'Contact dictionary successfully updated. Old phone replaced with {sanitize_phone_number(list[-1])}')
+        else:
+            return print('There are no match for your input. Type "show all" to see all contacts\n')
+    else:
+        return change(input('Function "change" accept only such sequence: first name,second name and phone number.Please try again\n'))
+
+def show_all():
+    text_phones = ''
+    for name, record in contacts_dictionary.items():
+        text_phones += name + ' ' + ', '.join([phone.value for phone in record.phones]) + '\n'
+    if len(text_phones)>0:
+        print(text_phones)
+    else:
+        print("There are no contacts yet.")
 
 def handler(string):
     exit_list=['good bye','close','exit']
     while string.lower() not in exit_list:
-        x=parser_command(string)
-        if len(x)==1:
-            if x[0].lower()=="hello":
-                x=input("Hello.How can I help you?:\n")
-                return handler(x)
-            else:
-                return handler(input("Looks like you type something wrong.Please try again,don't forget space between words and use correct commands\n"))
-        elif len(x)>1:
-            if x[0].lower()=="add":
-                if len(x)==4:
-                    list2=[]
-                    list2.extend(x[1:-1])
-                    clean_name=format_name(list2)
-                    if clean_name in contacts_dictionary:
-                        record = contacts_dictionary.data[clean_name]
-                        record.add_phone(sanitize_phone_number(x[-1]))
-                    else:
-                        record = Record(clean_name, sanitize_phone_number(x[-1]))
-                        contacts_dictionary.add_record(record)
-                    print(f'Contact dictionary successfully updated with such contact {clean_name}:{sanitize_phone_number(x[-1])}')
-                    return handler(input("Would you like to do something else?\n"))
-                else:
-                    return handler(input('Function "add" accept only such sequence: first name,second name and phone number.Please try again\n'))
-            elif x[0].lower()=="phone":
-                if len(x)<2 or len(x)>4:
-                    return handler(input('Function "change" accept only such sequence: first name and second name.Please try again\n'))
-                elif len(x) in [2,3]:
-                    list2=[]
-                    list2.extend(x[1:])
-                    phone_number=name_search(list2)
-                    if phone_number==None:
-                        return handler(input("Would you like to do something else?\n"))
-                    else:
-                        print(f'Contact has such number(s):{phone_number}')
-                        return handler(input("Would you like to do something else?\n"))
-            
-            elif x[0].lower()=="change":
-                if len(x)==4:
-                    list2=[]
-                    list2.extend(x[1:3])
-                    clean_name=format_name(list2)
-                    clean_name2=format_name(" ".join([list2[1],list2[0]]))
-                    if clean_name in contacts_dictionary.data.keys():
-                        old_phone=contacts_dictionary.data[clean_name]#list
-                        if len(old_phone.phones)>=2:
-                            old_telephone=input(f"There are few phones for {clean_name}.{[phone.value for phone in old_phone.phones]}.Type one you want to change.")
-                            old_phone.change_phone(sanitize_phone_number(old_telephone), sanitize_phone_number(x[-1]))
-                            print(f'Contact dictionary successfully updated. {old_telephone} replaced with {sanitize_phone_number(x[-1])}')
-                            return handler(input("Would you like to do something else?\n"))
-                        else:
-                            old_phone.change_phone(old_phone.phones[0].value, sanitize_phone_number(x[-1]))
-                            print(f'Contact dictionary successfully updated. Old phone replaced with {sanitize_phone_number(x[-1])}')
-                            return handler(input("Would you like to do something else?\n"))
-                    elif clean_name2 in contacts_dictionary.data.keys():
-                        old_phone=contacts_dictionary.data[clean_name2]#list
-                        if len(old_phone.phones)>=2:
-                            old_telephone=input(f"There are few phones for {clean_name2}.{[phone.value for phone in old_phone.phones]}.Type one you want to change.")
-                            old_phone.change_phone(sanitize_phone_number(old_telephone), sanitize_phone_number(x[-1]))
-                            print(f'Contact dictionary successfully updated. {old_telephone} replaced with {sanitize_phone_number(x[-1])}')
-                            return handler(input("Would you like to do something else?\n"))
-                        else:
-                            old_phone.change_phone(old_phone.phones[0].value, sanitize_phone_number(x[-1]))
-                            print(f'Contact dictionary successfully updated. Old phone replaced with {sanitize_phone_number(x[-1])}')
-                            return handler(input("Would you like to do something else?\n"))
-                    else:
-                        return handler(input('There are no match for your input. Type "show all" to see all contacts\n'))
-                else:
-                    return handler(input('Function "change" accept only such sequence: first name,second name and phone number.Please try again\n'))
+        parsed=parser_command(string)
+        if len(parsed)==1 and parsed[0].lower()=="hello":
+            return hello()
+        elif len(parsed)>1:
+            if parsed[0].lower()=="add":
+                add(parsed)
+                return handler(input("Would you like to do something else?\n"))
+            elif parsed[0].lower()=="phone":
+                phone_number(parsed)
+                return handler(input("Would you like to do something else?\n"))
+            elif parsed[0].lower()=="change":
+                change(parsed)
+                return handler(input("Would you like to do something else?\n"))
             elif string.lower() =="show all":
-                text_phones = ''
-                for name, record in contacts_dictionary.items():
-                    text_phones += name + ' ' + ', '.join([phone.value for phone in record.phones]) + '\n'
-                print(text_phones)
+                show_all()
                 return handler(input("Would you like to do something else?\n"))
         else:
             return handler(input("Looks like you type something wrong.Please try again,don't forget space between words\n"))
@@ -206,8 +219,8 @@ def main():
     print('[4] Enter "phone ....". With this command I will show number of contact that you enter.')
     print('[5] Enter "show all". With this command I will show all numbers existing in your contact list.')
     print('[6] Enter "good bye","close" or "exit" to quit.')
-    y = input('How can I help you today?:\n')
-    handler(y)
+    user_input_value = input('How can I help you today?:\n')
+    handler(user_input_value)
     print("Good bye!")
 if __name__ == '__main__':
     main()
